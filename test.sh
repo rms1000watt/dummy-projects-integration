6252f2f23e84806ee351990ca2844110d3b24051
Ryan Smith <rms1000watt@gmail.com>
2018-01-03 16:29:22 -0800

    updated tests

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 
 curl http://localhost:9999/pizza
+if [ $? -ne 0 ]; then
+  echo "cURL Golang failed!"
+  exit 1
+fi
+
 curl http://localhost:3000/
+if [ $? -ne 0 ]; then
+  echo "cURL NodeJS failed!"
+  exit 1
+fi
+
 echo 
